6ba33bb2f35c4347dc8b8d82ddde39befa60d007
Zhen Fang <1521442784@qq.com>
2024-02-25 15:21:36 -0800

    [Web_server] -Post

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const querystring = require('querystring');
 const port = 3000;
 const server = http.createServer((req, res) => {
     const method = req.method;
-    console.log('method', method);
-    const url = req.url;
-    console.log('url', url);
-    req.query = querystring.parse(url.split('?')[1]);
-    console.log(req.query);
-    res.end(JSON.stringify(req.query));
+    if (method === 'POST') {
+        let postData = '';
+        //stream
+        req.on('data', chunk => {
+            postData += chunk.toString();
+        })
+
+        req.on("end",()=>{
+            console.log('postData',postData);
+            res.end('Data received');
+        })
+
+        console.log('post data content type',req.headers['content-type']);
+    }
 });
 
 server.listen(port, () => {
